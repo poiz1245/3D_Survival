fed4959d1f2f036452878f6a94f0f7894ed697af
poiz1245 <wva960@gmail.com>
2024-06-06 16:39:54 +0900

    feat : 직선으로 몬스터를 향해 날아가도록 수정

--- 3D_Survival/Assets/Scripts/MainScripts/Bullet.cs
+++ 3D_Survival/Assets/Scripts/MainScripts/Bullet.cs
@@ -2,15 +2,19 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEditor.PlayerSettings;
 
 public class Bullet : MonoBehaviour
 {
+    public float speed;
+
+    [SerializeField] float damage;
+
     Transform targetPos;
     float time = 0;
     float maxDistance;
-
     private void Start()
     {
         maxDistance = GameManager.Instance.player.scanRadius;
@@ -29,7 +33,10 @@ public class Bullet : MonoBehaviour
             gameObject.SetActive(false);
             return;
         }
-        float distance = Vector3.Distance(transform.position, targetPos.position);
+
+        Vector3 moveDir = targetPos.position - transform.position;
+        transform.Translate(moveDir * speed * Time.deltaTime);
+        /*float distance = Vector3.Distance(transform.position, targetPos.position);
         float offsetY = Mathf.Lerp(1f, 2f, distance / maxDistance);
 
         Vector3 middlePoint = (transform.position + targetPos.position) / 2;
@@ -38,22 +45,16 @@ public class Bullet : MonoBehaviour
         Vector3 p1 = Vector3.Lerp(transform.position, thirdPoint, time);
         Vector3 p2 = Vector3.Lerp(thirdPoint, targetPos.position, time);
 
-        transform.position = Vector3.Lerp(p1, p2, time / 2);
+        transform.position = Vector3.Lerp(p1, p2, time / 2);*/
         time += Time.deltaTime;
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Monster"))
         {
             Monster monster = other.gameObject.GetComponent<Monster>();
-            monster.GetDamage(GameManager.Instance.player.damage / 2);
+            monster.GetDamage(damage);
             gameObject.SetActive(false);
-
-            if (monster.hp <= 0)
-            {
-                gameObject.SetActive(false);
-            }
         }
     }
 }
