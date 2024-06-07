a8eb400b07e4b87829a7154726154b1ef8c830e7
poiz1245 <wva960@gmail.com>
2024-06-08 00:46:54 +0900

    feat : 기본 Weapon클래스 정의
    - 각 weapon이 상속받을 기본 클래스 정의
    - upgrade메서드 구현

---
+++ 3D_Survival/Assets/Scripts/MainScripts/Weapon.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int level;
+    public float speed;
+    public float damage;
+    public float range;
+
+    public Weapon(int level, float speed, float damage, float range)
+    {
+        this.level = level;
+        this.speed = speed;
+        this.damage = damage;
+        this.range = range;
+    }
+    public virtual void WeaponUpGrade()
+    {
+        level++;
+        damage += 10;
+        speed *= 2;
+        range *= 1.5f; 
+    }
+}
