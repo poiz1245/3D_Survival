6ec2fd0dfaf1dfd7e44152b32e19e472f1c3746a
poiz1245 <wva960@gmail.com>
2024-06-10 00:27:24 +0900

    feat : 캐릭터 업그레이드 수치 조정

--- 3D_Survival/Assets/Scripts/MainScripts/SkillUpGrade.cs
+++ 3D_Survival/Assets/Scripts/MainScripts/SkillUpGrade.cs
@@ -8,47 +8,34 @@ public class SkillUpGrade : MonoBehaviour
     public void SowrdUp()
     {
         WeaponManager.instance.UpgradeWeapon(0);
-        //level1 : 스폰
-        //level2 : 스폰
-        //level3 : 회전스피드 증가
-        //level4 : 크기 증가
     }
     public void AoEUp()
     {
         WeaponManager.instance.UpgradeWeapon(1);
-        //level1 : 소환
-        //level2 : 범위 증가
-        //level4 : 데미지 증가
-        //level3 : 데미지 쿨타임 감소
-    }
-    public void MissileUp()
-    {
-        //level1 : 공속 증가
-        //level2 : 발사 방향 추가
-        //level3 : 데미지 증가
-        //level4 : 발사 방향 추가
     }
     public void HomingLaucherUp()
     {
-        //level1 : 소환
-        //level2 : 발사체 증가
-        //level3 : 발사 속도 증가
-        //level4 : 데미지 증가
+        WeaponManager.instance.UpgradeWeapon(2);
     }
+    public void MissileUp()
+    {
+        WeaponManager.instance.UpgradeWeapon(3);
+    }
+   
     public void PlayerMaxHpUp()
     {
-        GameManager.Instance.player.maxHp += 50;
+        GameManager.Instance.player.maxHp *= 1.3f;
     }
     public void PlayerPowerUp()
     {
-        GameManager.Instance.player.playerAttackPower += 10;
+        GameManager.Instance.player.playerAttackPower *= 1.1f;
     }
     public void PlayerShieldUp()
     {
-        GameManager.Instance.player.playerShield += 10;
+        GameManager.Instance.player.playerShield *= 1.1f;
     }
     public void PlayerScanRangeUp()
     {
-        GameManager.Instance.player.objectScanRadius += 2;
+        GameManager.Instance.player.objectScanRadius *= 1.3f;
     }
 }
