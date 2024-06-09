648b8cdb112e876cc022e9da6cf2ae48f467496d
poiz1245 <wva960@gmail.com>
2024-06-10 00:26:48 +0900

    fix : 레벨1일 때 오브젝트 소환 안되는 오류 수정
    - 오브젝트의 SetActive를 감지하는 이벤트 생성해서 레벨 1이 될 때 공격 시작

--- 3D_Survival/Assets/Scripts/AoE.cs
+++ 3D_Survival/Assets/Scripts/AoE.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static HomingLauncher;
 
 public class AoE : Weapon
 {
@@ -9,6 +10,9 @@ public class AoE : Weapon
 
     LayerMask targetLayer;
 
+    public delegate void ObjectVisibilityChangedHandler(bool isSpawn);
+    public event ObjectVisibilityChangedHandler OnObjectVisibilityChanged;
+
     public AoE(int level, float speed, float damage, float range, float coolTime) : base(level, speed, damage, range)
     {
         this.level = level;
@@ -16,13 +20,41 @@ public class AoE : Weapon
         this.damage = damage;
         this.coolTime = coolTime;
     }
+   
+    private void Start()
+    {
+        targetLayer = LayerMask.GetMask("Monster");
+        OnObjectVisibilityChanged += HandleVisibilityChanged;
+    }
+    void OnVisibilityChanged(bool isVisible)
+    {
+        OnObjectVisibilityChanged?.Invoke(isVisible);
+    }
+
+    private void HandleVisibilityChanged(bool isSpawn)
+    {
+        effact.gameObject.SetActive(true);
+        InvokeRepeating("Attack", 0f, coolTime);
+    }
+
+    void Attack()
+    {
+        Collider[] targets = Physics.OverlapSphere(transform.position, range, targetLayer);
+
+        foreach (Collider target in targets)
+        {
+            Monster monsterScript = target.GetComponent<Monster>();
+            monsterScript.GetDamage(damage);
+        }
+    }
+
     public override void WeaponUpGrade()
     {
         base.WeaponUpGrade();
         if (level == 1)
         {
             //º“»Ø
-            gameObject.SetActive(true);
+            OnVisibilityChanged(true);
         }
         else if (level == 2)
         {
@@ -42,22 +74,6 @@ public class AoE : Weapon
             coolTime *= 0.5f;
             mainModule.duration *= coolTime;
 
-        }
-    }
-    private void Start()
-    {
-        InvokeRepeating("Attack", 0f, coolTime);
-        targetLayer = LayerMask.GetMask("Monster");
-    }
-
-    void Attack()
-    {
-        Collider[] targets = Physics.OverlapSphere(transform.position, range, targetLayer);
-
-        foreach (Collider target in targets)
-        {
-            Monster monsterScript = target.GetComponent<Monster>();
-            monsterScript.GetDamage(damage);
         }
     }
 }
