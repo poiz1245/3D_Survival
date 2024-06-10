243fd38923fcdb9204e57efb781edc72ca3d9cf7
poiz1245 <wva960@gmail.com>
2024-06-10 15:56:26 +0900

    feat : 스킬 업그레이드 할 때 게임 다시 시작

--- 3D_Survival/Assets/Scripts/MainScripts/SkillUpGrade.cs
+++ 3D_Survival/Assets/Scripts/MainScripts/SkillUpGrade.cs
@@ -4,38 +4,53 @@ using UnityEngine;
 
 public class SkillUpGrade : MonoBehaviour
 {
-
     public void SowrdUp()
     {
+        Time.timeScale = 1.0f;
         WeaponManager.instance.UpgradeWeapon(0);
+        UIManager.Instance.buttonPanel.SetActive(false);
     }
     public void AoEUp()
     {
+        Time.timeScale = 1.0f;
         WeaponManager.instance.UpgradeWeapon(1);
+        UIManager.Instance.buttonPanel.SetActive(false);
     }
     public void HomingLaucherUp()
     {
+        Time.timeScale = 1.0f;
         WeaponManager.instance.UpgradeWeapon(2);
+        UIManager.Instance.buttonPanel.SetActive(false);
     }
     public void MissileUp()
     {
+        Time.timeScale = 1.0f;
         WeaponManager.instance.UpgradeWeapon(3);
+        UIManager.Instance.buttonPanel.SetActive(false);
     }
    
     public void PlayerMaxHpUp()
     {
+        Time.timeScale = 1.0f;
         GameManager.Instance.player.maxHp *= 1.3f;
+        UIManager.Instance.buttonPanel.SetActive(false);
     }
     public void PlayerPowerUp()
     {
+        Time.timeScale = 1.0f;
         GameManager.Instance.player.playerAttackPower *= 1.1f;
+        UIManager.Instance.buttonPanel.SetActive(false);
     }
     public void PlayerShieldUp()
     {
+        Time.timeScale = 1.0f;
         GameManager.Instance.player.playerShield *= 1.1f;
+        UIManager.Instance.buttonPanel.SetActive(false);
     }
     public void PlayerScanRangeUp()
     {
+        Time.timeScale = 1.0f;
         GameManager.Instance.player.objectScanRadius *= 1.3f;
+        UIManager.Instance.buttonPanel.SetActive(false);
     }
 }
