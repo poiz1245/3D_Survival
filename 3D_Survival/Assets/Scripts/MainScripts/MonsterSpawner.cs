bf322dd13c3f2e13c1ffbf9ee601f454ca4989cc
dltpdbs5715 <dltpdbs5715@gmail.com>
2024-06-12 11:51:03 +0900

    feat : 히트이펙트 에셋 추가, 몬스터 스포너 수정
    몬스터 스포너 쿨타임이 0 밑으로 내려가면 0.5로 변하게 수정

--- 3D_Survival/Assets/Scripts/MainScripts/MonsterSpawner.cs
+++ 3D_Survival/Assets/Scripts/MainScripts/MonsterSpawner.cs
@@ -29,7 +29,6 @@ public class MonsterSpawner : MonoBehaviour
        
         if (!isMaxStage)
         {
-            print("현재 스테이지:" + stage);
             if (stage == 0 || stage % 2 == 0)
             {
                 StartCoroutine(MeleeMonsterSpawn(stage, meleeSpawnDelay));
@@ -42,8 +41,7 @@ public class MonsterSpawner : MonoBehaviour
 
         if (isMaxStage)
         {
-            print("맥스 스테이지");
-            StartCoroutine(BossMonsterSpawn(stage, 0));
+            BossMonsterSpawn(stage, 0);
         }
 
     }
@@ -52,6 +50,13 @@ public class MonsterSpawner : MonoBehaviour
     {
         int rnd = Random.Range(0, spawnPoints.Length);
         float stageDelay = spawnDelay - (index * 0.5f);
+
+        if (stageDelay <= 0)
+        {
+            stageDelay = 0.5f;
+        }
+
+        print(stageDelay);
 
         while (true)
         {
@@ -67,6 +72,11 @@ public class MonsterSpawner : MonoBehaviour
         int rnd = Random.Range(0, spawnPoints.Length);
         float stageDelay = spawnDelay - (index * 0.5f);
 
+        if (stageDelay <= 0)
+        {
+            stageDelay = 0.5f;
+        }
+
         while (true)
         {
             yield return new WaitForSeconds(stageDelay);
@@ -75,13 +85,16 @@ public class MonsterSpawner : MonoBehaviour
         }
     }
 
-    IEnumerator BossMonsterSpawn(int index, float spawnDelay)
+    void BossMonsterSpawn(int index, float spawnDelay)
     {
-        print("보스몬스터소환");
         int rnd = Random.Range(0, spawnPoints.Length);
         float stageDelay = spawnDelay - (index * 0.5f);
 
-        yield return new WaitForSeconds(stageDelay);
+        if (stageDelay <= 0)
+        {
+            stageDelay = 0.5f;
+        }
+
         bossMonster = GameManager.Instance.monsterPool.GetMonster(2);
         bossMonster.transform.position = spawnPoints[rnd].position;
     }
