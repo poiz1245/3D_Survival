bff9fa1a39914f03eefbeaee0d210b7724908d81
poiz1245 <wva960@gmail.com>
2024-06-13 16:09:57 +0900

    fix ; 버튼 계속 눌리는 현상 수정
    - 버튼 한번 누르면 비활성화

--- 3D_Survival/Assets/Scripts/MainScripts/SceneController.cs
+++ 3D_Survival/Assets/Scripts/MainScripts/SceneController.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] Button button;
+
+    private void Start()
+    {
+        button.interactable = true;
+    }
     public void LoadGameScene()
     {
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene("Main");
+        button.interactable = false;
+        StartCoroutine(LoadSceneWithDelay("Main", 1.5f)); // 2ÃÊ Áö¿¬
     }
-
+    private IEnumerator LoadSceneWithDelay(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
     public void LoadTitleScene()
     {
         Time.timeScale = 1.0f;
