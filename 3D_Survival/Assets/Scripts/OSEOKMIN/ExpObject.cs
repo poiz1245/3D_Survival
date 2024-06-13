f9e57dbd3d3b0d9c3974e1d0eba6aec6e85e76a6
poiz1245 <wva960@gmail.com>
2024-06-13 16:09:23 +0900

    fix : 사운드 재생 안되는 현상 수정
    - 습득하고 비활성화 되기까지 delay추가

--- 3D_Survival/Assets/Scripts/OSEOKMIN/ExpObject.cs
+++ 3D_Survival/Assets/Scripts/OSEOKMIN/ExpObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class ExpObject : MonoBehaviour
@@ -9,8 +10,8 @@ public class ExpObject : MonoBehaviour
    
     [SerializeField] GameObject light;
     //
-    [SerializeField] AudioClip pickupSound; // 오디오 클립을 할당할 수 있는 필드
-    private AudioSource audioSource; // AudioSource 컴포넌트를 저장할 변수
+    //[SerializeField] AudioClip pickupSound; // 오디오 클립을 할당할 수 있는 필드
+    [SerializeField] AudioSource audioSource; // AudioSource 컴포넌트를 저장할 변수
     //
     float rotationSpeed = 100f;
     int amount;
@@ -18,14 +19,10 @@ public class ExpObject : MonoBehaviour
     private void Awake()
     {
         playerTransform = GameManager.Instance.player.transform;
-        //
-        audioSource = GetComponent<AudioSource>(); // AudioSource 컴포넌트 가져오기
-        //
     }
     private void Update()
     {
         transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f, Space.World);
-        //light.transform.Rotate(0f, -rotationSpeed * Time.deltaTime,0f );
     }
     public void SetAmount(int expAmount) //몬스터 죽을 때 몬스터가 가진 경험치 값을 오브젝트에 할당받는 함수
     {
@@ -36,19 +33,19 @@ public class ExpObject : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            audioSource.Play(); //소리 왜 재생 안되는지 원인불명
             GameManager.Instance.player.AddExperience(amount);
-            //
-            PlayPickupSound(); // 소리 재생
-            //
-            gameObject.SetActive(false);
+            InVisibleObject(0.5f);
         }
     }
-    private void PlayPickupSound()
+   IEnumerator InVisibleObjectWithDelay(float delay)
     {
-        if (pickupSound != null && audioSource != null)
-        {
-            audioSource.PlayOneShot(pickupSound);
-        }
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }
+    void InVisibleObject(float delay)
+    {
+        StartCoroutine(InVisibleObjectWithDelay(delay));
     }
     public void MoveToPlayer()
     {
